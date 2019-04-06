ace73f338a8b6bbe46f849d539bab09f24a980d3
fjerbaek <jacob.f.olsen@gmail.com>
2019-04-06 11:04:24 +0200

    Næsten klar til json

--- app.js
+++ app.js
@@ -3,18 +3,62 @@ const app = express();
 const port = 3000;
 const pug = require('pug');
 const http = require('http');
+const apiurl = "http://snapsecounter.serveo.net/"
+const axios = require('axios')
+
+function setheader(token) {
+	return {authorization : "Token " + token}
+}
+
+var token = "";
 
 app.set('view engine', 'pug');
 
-app.get('/', (req, res) => {
-	res.render('index.pug');
+app.get('/', (req, res) => {		
+	if (!isLoggedIn()) {
+		login("test", "test", res);
+	}
+	res.render('index.pug',{events:[1,2,3,4]});
 });
 
+function isLoggedIn(){
+	return token != "";
+}
+
 app.get('/event/:eventID', (req,res) => {
-	http.get('http://amor.serveo.net/api/events/' + req.params.eventID, (resp) => {
-		res.send(JSON.stringify(resp, null, 2));
+	let request = http.get(apiurl + "api/events/", function(response){
+		let body = ""; 
+		response.on("data", function(chunk){ body += chunk; });
+
+		response.on("end", function(){ 
+			if(response.statusCode === 200){ 
+				try{ var profile = JSON.parse(body);
+					res.send(JSON.stringify(profile));
+				} 
+				catch(error){
+					console.log(error);
+				}
+
+			};
+		});
+	});
+});
+
+function login(uname, pass, loginres){
+	console.log("Started login")
+	body = "{username : " + uname + ", password : " + pass + "}"
+	axios.post(apiurl + "api-token-auth/",{
+		username : uname,
+		password : pass
 	})
-
-})
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+		.then((res) => {
+			if (res.status == 200) {
+				console.log(res.data.token);
+				token = res.data.token;
+			}
+		})
+		.catch((err) => {
+			loginres.redirect("/?err=login")
+		});
+}
+app.listen(port, () => console.log("Listening on port ${port}!"));
