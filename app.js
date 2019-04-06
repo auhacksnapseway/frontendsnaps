81e9400d4a2dd8b98bf1b22470997ab79b94f0ef
fjerbaek <jacob.f.olsen@gmail.com>
2019-04-06 02:07:12 +0200

    Har lavet en server der fejler

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const pug = require('pug');
+const http = require('http');
 
 app.set('view engine', 'pug');
 
@@ -10,7 +11,10 @@ app.get('/', (req, res) => {
 });
 
 app.get('/event/:eventID', (req,res) => {
-	res.send(req.params.eventID);;
+	http.get('http://amor.serveo.net/api/events/' + req.params.eventID, (resp) => {
+		res.send(JSON.stringify(resp, null, 2));
+	})
+
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
